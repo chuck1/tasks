6cb35e117278a60b42c2bd3a33d13cf9245c7a75
Charles Rymal <charles.rymal@nortek.com>
2017-10-02 09:59:27 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import re
+import json
+from setuptools import setup
+
+c = {'name':'todo',
+        'description':'',
+        'url':'github.com/chuck1/todo',
+        'author':'charles rymal',
+        'author_email':'',
+        'license':'',
+        }
+
+with open(os.path.join(c['name'], '__init__.py')) as f:
+    version = re.findall("^__version__ = '(.*)'", f.read())[0]
+
+kwargs = {
+        'name': c['name'],
+        'version': version,
+        'description': c['description'],
+        'url': c['url'],
+        'author': c['author'],
+        'author_email': c['author_email'],
+        'license': c['license'],
+        'packages': [],
+        'zip_safe': False,
+        'install_requires': [],
+        }
+
+setup(**kwargs)
+
+
+
